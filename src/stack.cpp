7ed093818fbcaf5bd2637f771609e621ed87d15c
Anastasia <kulevich05@gmail.com>
2022-09-25 22:52:01 +0300

    Add assertions.

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "stack.h"
 
 void stack_ctor(stack_t *stack, size_t capacity)
 {
+        assert(stack);
+
         stack->data = (elem_t *) calloc(capacity, sizeof(elem_t));
 }
 
 void stack_push(stack_t *stack, elem_t elem)
 {
-        stack->data[stack->size] = elem;
-        stack->size++;
+        assert(stack);
+
+        if (size < capacity) {
+                stack->data[stack->size] = elem;
+                stack->size++;
+        }
 }
 
 elem_t stack_pop(stack_t *stack)
 {
+        assert(stack);
+
         stack->size--;
         return stack->data[stack->size];
 }
 
 void stack_dtor(stack_t *stack)
 {
+        assert(stack);
+
         free(stack->data);
 }
